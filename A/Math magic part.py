a6262e2a413ea2232352fb06e9f9357cd889fe05
trada17 <iamnottrada17@gmail.com>
2025-03-23 09:50:28 +0100

    Add files via upload

---
+++ A/Math magic part.py
@@ -0,0 +1,15 @@
+#CHECKING FOR PRIME NUMBERS
+from math input sqrt
+
+number=int(input("enter a number to check:"))
+print("\n")
+
+if number>:1
+    for i in range(2,int(sqrt(number))+1):
+        if(number%1)==0:
+            print(f"{number} is not a prime")
+            break
+    else:
+            print(f"{number} is a prime")
+else:
+        print(f"{number} is not a prime")
---
+++ A/main 2.py
@@ -0,0 +1,15 @@
+def sieveOfEratosthenes(num):
+    prime=[True for i in range(num+1)]
+    p=2
+    while (p*p<=num):
+        if (prime[p]==True):
+            for i in range(p*p,num+1,p):
+                prime[i]=False
+        p+=1
+    for p in range(2,num+1):
+        if prime[p]:
+            print (p,end=" ")
+
+num=int(input("enter a number: "))
+print("the following are prime numbers smaller than or equal to", num)
+sieveOfEratosthenes(num)
